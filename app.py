c18676a46a54b8408927eb3378ca7c2e2d379e13
ucanvn <ezwsoft@gmail.com>
2024-10-31 10:09:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -29,4 +30,5 @@ def contact_china():
     return render_template('contact_china.html')
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', debug=True)
